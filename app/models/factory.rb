7dd2ca3c73a0da2b18f0826625f717147576a2c3
Mark L. Chang <mark.chang@gmail.com>
2011-02-04 15:41:29 -0500

    validated factory model with rspec

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,2 +1,17 @@
 class Factory < ActiveRecord::Base
+  validates :name, :location, :presence => true
+  validates :name, :uniqueness => true
+  validates :name, :length => {:maximum => 20}
 end
+
+# == Schema Information
+#
+# Table name: factories
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  location   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- spec/models/factory_spec.rb
+++ spec/models/factory_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Factory do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should require a name" do
+    something = Factory.create(:name => '', :location => "somewhere")
+    something.should_not be_valid
+  end
+  
+  it "should require a location" do
+    something = Factory.create(:name => 'name', :location => '')
+    something.should_not be_valid
+  end
+  
+  it "should have a unique name" do
+    first = Factory.create(:name => 'first', :location => 'here')
+    second = Factory.create(:name => 'first', :location => 'here')
+    
+    first.should be_valid
+    second.should_not be_valid
+  end
+  
+  it "should have a name with a maximum 20 characters" do
+    something = Factory.create(:name => '123456789012345678901', :location => 'here')
+    
+    something.should_not be_valid
+  end
+
 end
+
+# == Schema Information
+#
+# Table name: factories
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  location   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
