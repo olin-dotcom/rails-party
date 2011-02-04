44a1c4c277e47b07e14e1a69c2d84618b39231ac
Mark L. Chang <mark.chang@gmail.com>
2011-02-04 13:33:57 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsParty::Application.config.secret_token = 'e63410a16034d7e14dcf81f23914374064d4e3fa96b671d8476b2437d66daf51473912071d584051a92637612c15bf285e13cdd503290fd0c73d1f7ca1c5b29b'
